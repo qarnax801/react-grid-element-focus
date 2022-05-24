b1f22ed7418854c967ef119659f7e1afa8bc5fde
qarnax801 <k.y.wissempai@gmail.com>
2022-05-24 20:16:41 +0100

    Full code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [active, setActive] = useState(null);
+  const [inside, setInside] = useState(false);
+
+  const handleHover = (index) => {
+    setActive(index);
+  }
+
+  const cells = [
+    {index: 0, title: 'Element ONE'},
+    {index: 1, title: 'Element TWO'},
+    {index: 2, title: 'Element THREE'},
+    {index: 3, title: 'Element FOUR'}
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Grid element focus</h1>
+
+      {/* The trick here is to keep the behaviour
+        inside the container. When you leave the container,
+        everything gets back to normal :) */}
+      <div
+      className='container'
+      onMouseOver={() => setInside(true)}
+      onMouseOut={() => setInside(false)}
+      >
+        {cells.map((cell, index) =>
+          <div
+          key={cell.index}
+          onMouseOver={() => {
+            handleHover(index);
+          }}
+          className={`cell ${index === active ? "active" : "notActive"} ${
+          inside ? "" : "outOfBounds "
+          }`}>
+            {cell.title}
+          </div>
+        )}
+      </div>
+      <p>
+        This feature allows you to focus on an element from
+        a grid (or list or anything else) while blurring other elements.
+        The trick to get everything back to normal outside of bounds
+        is to just detect if the mouse is outside the container.
+        <br/>P.S. This can also be used to apply different stylings, not just
+        a blur effect.
+        Let me know if you have any issues or suggestions for making it better :)
+      </p>
+    </>
   );
 }
 
